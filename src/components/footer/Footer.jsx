38986e78430f9c427cb2719acebc93c3e6b2afc4
gokarna123-goku <agokarna444@gmail.com>
2023-03-21 08:59:20 +0545

    changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer>
       <a href="" className="footer__logo">
-        MANOHAR
+        Gokarna Adhikari
       </a>
 
       <ul className="permalinks">
